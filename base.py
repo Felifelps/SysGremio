41685de7cf18f2d1a53c31856b5d2571a11e98fd
Felifelps <felipefelipe23456@gmail.com>
2022-05-15 11:49:07 -0300

    Changing all again

--- base.py
+++ base.py
@@ -1,33 +1,16 @@
 
-class party:
-    def __init__(self, name):
-        self.name = name
-        self.points = 0
-
-    def one(self):
-        self.points += 1
-
-class election:
-    def __init__(self, partys):
-        self.partys = partys
-
-    def vote(self, partyNumber):
-        self.partys[partyNumber - 1].one()
-
-    def finalize(self):
-        output = ""
-        for party in self.partys:
-            output += party.name + ": " + str(party.points) + (" votos" if party.points != 1 else "  voto") + "\n"
-        return output
-
 
 class arq:
     def __init__(self, path):
         self.path = path
-        with open(self.path, "w") as arq:
-            arq.write("")
-        with open(self.path, "r") as arq:
-            read = arq.readlines()
+        try:
+            with open(self.path, "r") as arq:
+                read = arq.readlines()
+        except:
+            with open(self.path, "w") as arq:
+                arq.write("")
+            with open(self.path, "r") as arq:
+                read = arq.readlines()
         self.content = "".join(read)
 
     def read(self, string=1):
@@ -52,12 +35,56 @@ class arq:
         self.content = "".join(read)
 
 
+class party:
+    def __init__(self, name):
+        self.name = name
+        self.points = 0
 
-source = arq("source.txt")
-fd = party("FD - Força Democrática")
-puma = party("PUMA - Para Um Melhor Aprendizado")
-c3 = party("Chapa 3")
+    def one(self):
+        self.points += 1
 
-e = election([fd, puma, c3])
-e.vote(3)
-print(e.finalize())
+
+class election:
+    def __init__(self):
+        ps = arq("chapas.txt")
+        self.partys = []
+        if ps.content == "":
+            print("Sem chapas")
+            return None
+        else:
+            for i in ps.read(0):
+                p = party(i.replace("\n", ""))
+                self.partys.append(p)
+
+    def vote(self, pid):
+        n = 1
+        if self.partys == []:
+            print("Sem chapas")
+            return 0
+        elif pid < 1 or pid > len(self.partys):
+            text = "\nID fora do alcance\n$IDs das Chapas"
+            for i in self.partys:
+                text += "\n [" + i.name + "] : " + str(n)
+                n += 1
+            print(text + "\n")
+        else:
+            for p in self.partys:
+                if pid == n:
+                    p.one()
+                    return 0
+                n += 1
+            r = arq("progress.txt")
+            r.write(self.progress())
+                    
+
+    def progress(self):
+        output = ""
+        for p in self.partys:
+            output += p.name + ": " + str(p.points) + (" votos" if p.points != 1 else "  voto") + "\n"
+        return output
+
+
+e = election()
+for i in range(1, 10):
+    e.vote(2)
+print(e.progress())
